de74e7adcbb8da30e102ee1599b5414354e82be2
Harry Loud <admin@muon3d.com>
2024-10-11 12:03:29 +0100

    Bed Disconnected Overlay Working

--- src/components/widgets/thermals/TemperatureCard.vue
+++ src/components/widgets/thermals/TemperatureCard.vue
@@ -1,36 +1,14 @@
 <template>
-  <collapsable-card
-    :title="$t('app.general.title.temperature')"
-    icon="$fire"
-    :help-tooltip="$t('app.chart.tooltip.help')"
-    :lazy="false"
-    draggable
-    layout-path="dashboard.temperature-card"
-  >
+  <collapsable-card :title="$t('app.general.title.temperature')" icon="$fire"
+    :help-tooltip="$t('app.chart.tooltip.help')" :lazy="false" draggable layout-path="dashboard.temperature-card">
     <template #menu>
       <app-btn-collapse-group :collapsed="menuCollapsed">
-        <temperature-presets-menu
-          @applyOff="handleApplyOff"
-          @applyPreset="handleApplyPreset"
-        />
+        <temperature-presets-menu @applyOff="handleApplyOff" @applyPreset="handleApplyPreset" />
       </app-btn-collapse-group>
 
-      <v-menu
-        bottom
-        left
-        offset-y
-        transition="slide-y-transition"
-        :close-on-content-click="false"
-      >
+      <v-menu bottom left offset-y transition="slide-y-transition" :close-on-content-click="false">
         <template #activator="{ on, attrs }">
-          <v-btn
-            fab
-            x-small
-            text
-            v-bind="attrs"
-            class="ms-1 my-1"
-            v-on="on"
-          >
+          <v-btn fab x-small text v-bind="attrs" class="ms-1 my-1" v-on="on">
             <v-icon>
               $cog
             </v-icon>
@@ -96,24 +74,27 @@
       </v-menu>
     </template>
 
-    <temperature-targets
-      @legendClick="legendToggleSelect"
-      @legendPowerClick="legendTogglePowerSelect"
-    />
+    <div class="temperature-content">
+      <div class="temperature-container" :class="{ bedRemoved: bedRemoved }">
+        <!-- Temperature Targets Component -->
+        <temperature-targets @legendClick="legendToggleSelect" @legendPowerClick="legendTogglePowerSelect" />
 
-    <template v-if="chartReady && chartVisible">
-      <v-divider />
+        <!-- Thermal Chart Component -->
+        <template v-if="chartReady && chartVisible">
+          <v-divider />
+          <thermal-chart ref="thermalchart" :height="isMobileViewport ? '180px' : '260px'" />
+        </template>
+      </div>
+      <div class="bed-overlay" :class="{ bedRemoved: bedRemoved }">
+        <p>Bed Removed</p>
+      </div>
+    </div>
 
-      <thermal-chart
-        ref="thermalchart"
-        :height="(isMobileViewport) ? '180px' : '260px'"
-      />
-    </template>
   </collapsable-card>
 </template>
 
 <script lang="ts">
-import { Component, Mixins, Prop, Ref } from 'vue-property-decorator'
+import { Component, Mixins, Prop, Ref, Watch } from 'vue-property-decorator'
 import StateMixin from '@/mixins/state'
 import BrowserMixin from '@/mixins/browser'
 import type { Fan, Heater } from '@/store/printer/types'
@@ -137,7 +118,27 @@ export default class TemperatureCard extends Mixins(StateMixin, BrowserMixin) {
   @Ref('thermalchart')
   readonly thermalChartElement!: ThermalChart
 
-  get chartReady () {
+
+  bedRemoved: boolean = false;
+
+  // Watch the Vuex store's bedRemoved property
+  @Watch('$store.state.printer.printer.bed_removal_detector.bedRemoved', { immediate: true })
+  onBedRemovedChange(newVal: boolean) {
+    console.log('bedRemoved', newVal);
+    this.bedRemoved = newVal; // Update the reactive property
+  }
+
+
+
+  get chartReady() {
+    // try {
+    //   // @ts-ignore
+    //   console.log("LOGGING", this.$store.state.printer.printer.bed_removal_detector.bedRemoved);
+    // } catch (error) {
+    //   console.log("err");
+    // }
+
+
     return (
       this.$store.state.socket.acceptingNotifications &&
       this.$store.state.socket.ready &&
@@ -146,7 +147,7 @@ export default class TemperatureCard extends Mixins(StateMixin, BrowserMixin) {
     )
   }
 
-  legendToggleSelect (item: Heater | Fan) {
+  legendToggleSelect(item: Heater | Fan) {
     // If this has a target, toggle that too.
     if (this.chartVisible) {
       if ('target' in item) {
@@ -156,7 +157,7 @@ export default class TemperatureCard extends Mixins(StateMixin, BrowserMixin) {
     }
   }
 
-  legendTogglePowerSelect (item: Heater | Fan) {
+  legendTogglePowerSelect(item: Heater | Fan) {
     if (this.chartVisible) {
       const name = ('speed' in item)
         ? item.key + 'Speed'
@@ -165,11 +166,11 @@ export default class TemperatureCard extends Mixins(StateMixin, BrowserMixin) {
     }
   }
 
-  get chartVisible () {
+  get chartVisible() {
     return this.$store.state.config.uiSettings.general.chartVisible
   }
 
-  set chartVisible (value: boolean) {
+  set chartVisible(value: boolean) {
     this.$store.dispatch('config/saveByPath', {
       path: 'uiSettings.general.chartVisible',
       value,
@@ -177,11 +178,11 @@ export default class TemperatureCard extends Mixins(StateMixin, BrowserMixin) {
     })
   }
 
-  get showRateOfChange () {
+  get showRateOfChange() {
     return this.$store.state.config.uiSettings.general.showRateOfChange
   }
 
-  set showRateOfChange (value: boolean) {
+  set showRateOfChange(value: boolean) {
     this.$store.dispatch('config/saveByPath', {
       path: 'uiSettings.general.showRateOfChange',
       value,
@@ -189,11 +190,11 @@ export default class TemperatureCard extends Mixins(StateMixin, BrowserMixin) {
     })
   }
 
-  get showRelativeHumidity () {
+  get showRelativeHumidity() {
     return this.$store.state.config.uiSettings.general.showRelativeHumidity
   }
 
-  set showRelativeHumidity (value: boolean) {
+  set showRelativeHumidity(value: boolean) {
     this.$store.dispatch('config/saveByPath', {
       path: 'uiSettings.general.showRelativeHumidity',
       value,
@@ -201,11 +202,11 @@ export default class TemperatureCard extends Mixins(StateMixin, BrowserMixin) {
     })
   }
 
-  get showBarometricPressure () {
+  get showBarometricPressure() {
     return this.$store.state.config.uiSettings.general.showBarometricPressure
   }
 
-  set showBarometricPressure (value: boolean) {
+  set showBarometricPressure(value: boolean) {
     this.$store.dispatch('config/saveByPath', {
       path: 'uiSettings.general.showBarometricPressure',
       value,
@@ -213,11 +214,11 @@ export default class TemperatureCard extends Mixins(StateMixin, BrowserMixin) {
     })
   }
 
-  get showGasResistance () {
+  get showGasResistance() {
     return this.$store.state.config.uiSettings.general.showGasResistance
   }
 
-  set showGasResistance (value: boolean) {
+  set showGasResistance(value: boolean) {
     this.$store.dispatch('config/saveByPath', {
       path: 'uiSettings.general.showGasResistance',
       value,
@@ -225,7 +226,7 @@ export default class TemperatureCard extends Mixins(StateMixin, BrowserMixin) {
     })
   }
 
-  handleApplyPreset (preset: TemperaturePreset) {
+  handleApplyPreset(preset: TemperaturePreset) {
     if (preset) {
       if (preset.values) {
         for (const key in preset.values) {
@@ -245,7 +246,7 @@ export default class TemperatureCard extends Mixins(StateMixin, BrowserMixin) {
     }
   }
 
-  async handleApplyOff () {
+  async handleApplyOff() {
     const result = (
       !['printing', 'busy', 'paused'].includes(this.$store.getters['printer/getPrinterState']) ||
       await this.$confirm(
@@ -259,4 +260,53 @@ export default class TemperatureCard extends Mixins(StateMixin, BrowserMixin) {
     }
   }
 }
+
+
+
 </script>
+
+
+
+<style scoped lang="scss">
+.temperature-content {
+  position: relative; // Establish a positioning context for absolute children
+}
+
+.temperature-container{
+  transition: opacity 0.3s ease;
+  opacity: 1;
+  &.bedRemoved{
+    opacity: 0; // Show overlay when bed is removed
+    pointer-events: auto; // Prevent interactions with underlying elements when visible
+  }
+}
+
+.bed-overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(255, 0, 0, 0); // Semi-transparent red
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+  font-size: 1.2em;
+  font-weight: bold;
+  z-index: 10; // Ensure it sits above other elements
+  opacity: 0; // Hidden by default
+  pointer-events: none; // Allow interactions with underlying elements when hidden
+  transition: opacity 0.3s ease;
+
+
+  p {
+    text-align: center;
+  }
+
+  &.bedRemoved {
+    opacity: 1; // Show overlay when bed is removed
+    pointer-events: auto; // Prevent interactions with underlying elements when visible
+  }
+}
+</style>
